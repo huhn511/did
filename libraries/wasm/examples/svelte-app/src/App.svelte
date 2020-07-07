c01429e902803c2a28020fb960f01c9162b79ec9
huhn511 <sehe89@gmail.com>
2020-07-08 01:15:59 +0200

    :apple: add svelte example

---
+++ libraries/wasm/examples/svelte-app/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	import * as did from 'iota-did-wasm/wasm-web'
+	console.log("did", did)
+
+	export let name;
+	export let seed;
+	try {
+		seed = did.GenerateSeed();
+	} catch (error) {
+		seed = error
+	}
+	console.log("seed", seed)
+
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Seed: {seed}</p>
+</main>
